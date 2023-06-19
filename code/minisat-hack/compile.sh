a6784d71281619d2e89dc82a5e49661bfcbbbc91
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-19 20:06:14 +0100

    :zap: Update the g++ compiler from 4.8.5 to 9.3.1

--- code/minisat-hack/compile.sh
+++ code/minisat-hack/compile.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-MROOT=.. 
-CFLAGS=-fpermissive
-CXX=/opt/rh/devtoolset-9/root/usr/bin/gcc
-make -C simp
+MROOT=.. CFLAGS=-fpermissive CXX=/opt/rh/devtoolset-9/root/usr/bin/g++ make -C simp
