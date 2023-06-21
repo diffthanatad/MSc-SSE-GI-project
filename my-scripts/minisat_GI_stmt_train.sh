02276eb5ba1a06733708cac3327deacf8b7f6c1b
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-21 22:12:45 +0100

    :bug: Fix the directory path in the minisat_GI_stmt_train.sh file.

--- my-scripts/minisat_GI_stmt_train.sh
+++ my-scripts/minisat_GI_stmt_train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
-../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
