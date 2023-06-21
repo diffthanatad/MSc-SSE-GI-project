4be1a063c65eaf2d95102d336f6cf5f2139204bd
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-21 22:08:48 +0100

    :zap: Run all minisat_GI_stmt_train_1-10.sh.

--- my-scripts/minisat_GI_stmt_train.sh
+++ my-scripts/minisat_GI_stmt_train.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-END=10
-for i in $(seq 1 $END); 
-do
-    CMD="./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_${i}.txt"
-    $CMD; 
-done
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
+../magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
