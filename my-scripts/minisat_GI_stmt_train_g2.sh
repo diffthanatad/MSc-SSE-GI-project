580309d2725fd9ef1ae2a66f0db45999aa81def0
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-23 20:31:46 +0100

    :zap: Prepare for experiment. 2 groups for minisat GI training.

---
+++ my-scripts/minisat_GI_stmt_train_g2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
