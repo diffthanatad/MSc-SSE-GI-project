2615b21def5fe852cf2d330f08cd1226b3f0d7cc
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-23 22:00:57 +0100

    :hammer: Refactor code.

--- my-scripts/minisat_GI_stmt_train.sh
+++ my-scripts/minisat_GI_stmt_train.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
@@ -8,5 +10,3 @@
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
