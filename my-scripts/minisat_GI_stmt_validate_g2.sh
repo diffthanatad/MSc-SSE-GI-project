6bd55a41e615c1dc6459b76a83500de13d326435
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-23 21:30:05 +0100

    :zap: Prepare for validation phase of minisat.

---
+++ my-scripts/minisat_GI_stmt_validate_g2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_2.txt --patch path/to/my/patch
+./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_4.txt --patch path/to/my/patch
+./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_6.txt --patch path/to/my/patch
+./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_7.txt --patch path/to/my/patch
+./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_8.txt --patch path/to/my/patch
