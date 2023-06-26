561a9a7fa807f961e6d569e08cc9f893624a9ee6
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-26 23:08:16 +0100

    :zap: Divide work load into 2 groups. Each has 5 experiments running concurrently.

--- my-scripts/minisat_GI_stmt_train_g2.sh
+++ my-scripts/minisat_GI_stmt_train_g2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt > console-output/nohup_train_2.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt > console-output/nohup_train_4.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt > console-output/nohup_train_6.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt > console-output/nohup_train_7.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt > console-output/nohup_train_8.txt &
