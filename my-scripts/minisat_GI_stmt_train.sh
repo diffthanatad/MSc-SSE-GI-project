38b7ded1ed845cc8a94b968bcfb4641addc88588
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-26 16:51:06 +0100

    :bug: Reconfigure the *.sh file from line to statement.

--- my-scripts/minisat_GI_stmt_train.sh
+++ my-scripts/minisat_GI_stmt_train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt > console-output/nohup_train_1.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt > console-output/nohup_train_2.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt > console-output/nohup_train_3.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt > console-output/nohup_train_4.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt > console-output/nohup_train_5.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt > console-output/nohup_train_6.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt > console-output/nohup_train_7.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt > console-output/nohup_train_8.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt > console-output/nohup_train_9.txt &
+nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt > console-output/nohup_train_10.txt &
