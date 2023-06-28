227b91bf71c3f596fc48bcc72d3421b4f916703c
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-06-28 21:18:28 +0100

    :page_facing_up: Trial with WSL experiment run. 10-k <=2 concurrent run. ~13 hours.

--- my-scripts/minisat_GI_stmt_train_g1.sh
+++ my-scripts/minisat_GI_stmt_train_g1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt > console-output/nohup_train_1.txt &
-nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt > console-output/nohup_train_3.txt &
-nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt > console-output/nohup_train_5.txt &
-nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt > console-output/nohup_train_9.txt &
-nohup ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt > console-output/nohup_train_10.txt &
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt 
--- my-scripts/minisat_GI_stmt_train_g2.sh
+++ my-scripts/minisat_GI_stmt_train_g2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_2.txt > console-output/nohup_train_2.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_4.txt > console-output/nohup_train_4.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_6.txt > console-output/nohup_train_6.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_7.txt > console-output/nohup_train_7.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_8.txt > console-output/nohup_train_8.txt &
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
+./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
