30e98433bc289d73a3e87060db934ebd126b66cb
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-05 09:10:50 +0100

    :page_facing_up: Minisat Stmt 16-cores

--- my-scripts/minisat_GI_stmt_train_g1.sh
+++ my-scripts/minisat_GI_stmt_train_g1.sh
@@ -5,3 +5,10 @@
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
 ./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt 
+
+nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_1.txt > console-output/nohup_train_1.txt &
+nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_3.txt > console-output/nohup_train_3.txt &
+nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_5.txt > console-output/nohup_train_5.txt &
+nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_9.txt > console-output/nohup_train_9.txt &
+nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_10.txt > console-output/nohup_train_10.txt &
+
