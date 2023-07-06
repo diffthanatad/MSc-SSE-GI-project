4767039173e9ca58bcd704e9a7a04a11923aafb0
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-06 14:46:51 +0100

    :zap: Upload group script file for concurrent run.

---
+++ my-scripts/minisat_AC_train_g2.sh
@@ -0,0 +1,5 @@
+
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_4.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_5.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_6.txt
+
---
+++ my-scripts/minisat_AC_train_g3.sh
@@ -0,0 +1,6 @@
+
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_7.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_8.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_9.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_10.txt
+
