0a96eedc9cbb06665fc65658277e4c35fbdfa582
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-06 15:26:40 +0100

    :zap: Update path files.

--- my-scripts/minisat_AC_train_g1.sh
+++ my-scripts/minisat_AC_train_g1.sh
@@ -1,5 +1,5 @@
 
-/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_1.txt
-/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_2.txt
-/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_AC_train_3.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_1.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_2.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_3.txt
 
