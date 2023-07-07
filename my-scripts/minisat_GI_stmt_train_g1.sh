7206f14550d5e6d189852055f8b81b2d722aaa65
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-07 15:44:10 +0100

    :zap: Update scripts

--- my-scripts/minisat_GI_stmt_train_g1.sh
+++ my-scripts/minisat_GI_stmt_train_g1.sh
@@ -1,14 +1,2 @@
-#!/bin/sh
-
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_1.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_3.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_5.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_9.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_10.txt 
-
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_1.txt > console-output/nohup_train_1.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_3.txt > console-output/nohup_train_3.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_5.txt > console-output/nohup_train_5.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_9.txt > console-output/nohup_train_9.txt &
-nohup /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/minisat_GI_stmt_train_10.txt > console-output/nohup_train_10.txt &
-
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/GI-stmt/minisat_GI_stmt_train_2.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/GI-stmt/minisat_GI_stmt_train_3.txt
--- my-scripts/minisat_GI_stmt_train_g2.sh
+++ my-scripts/minisat_GI_stmt_train_g2.sh
@@ -1,7 +1,2 @@
-#!/bin/sh
-
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_2.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_4.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_6.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_7.txt
-./magpie.py local_search --scenario scenario/minisat_GI_stmt_train_8.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/GI-stmt/minisat_GI_stmt_train_5.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/GI-stmt/minisat_GI_stmt_train_6.txt
