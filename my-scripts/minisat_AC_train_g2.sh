6d945bd0b7b47ede4ccaafceda17030b8e8af776
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-07 08:15:58 +0100

    :bug: Fix the wrong bin argument.

--- my-scripts/minisat_AC_train_g2.sh
+++ my-scripts/minisat_AC_train_g2.sh
@@ -1,2 +1,2 @@
 
-/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_4.txt
+/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_4.txt
