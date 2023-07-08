67002289b8c0e7b60cb25fdb8167b5c7a7a283cc
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-08 08:29:47 +0100

    :zap: Update script file.

--- my-scripts/g4.sh
+++ my-scripts/g4.sh
@@ -1,1 +1,2 @@
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/GI-stmt/minisat_GI_stmt_train_9.txt
 /usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_6.txt
