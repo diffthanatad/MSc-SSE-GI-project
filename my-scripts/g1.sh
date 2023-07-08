56a9d61897bafcd21af278710186d77492a747c5
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-08 08:22:12 +0100

    :bug: Fix the script bin.*

--- my-scripts/g1.sh
+++ my-scripts/g1.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_2.txt
-/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_7.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_2.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_7.txt
--- my-scripts/g3.sh
+++ my-scripts/g3.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_5.txt
-/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_9.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_5.txt
+/usr/local/bin/python3.10 -m bin.local_search --scenario scenario/AC/minisat_AC_train_9.txt
