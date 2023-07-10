83f7a038599c120d0128e6429b4672143459fd0c
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-10 10:43:38 +0100

    :zap: Upload script for AC validation.

--- my-scripts/minisat_AC_train_g3.sh
+++
@@ -1,2 +0,0 @@
-
-/usr/local/bin/python3.10 -m bin.evolution_algorithm --scenario scenario/AC/minisat_AC_train_10.txt
--- my-scripts/minisat_GI_stmt_validate_g1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_1.txt --patch path/to/my/patch
-./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_3.txt --patch path/to/my/patch
-./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_5.txt --patch path/to/my/patch
-./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_9.txt --patch path/to/my/patch
-./magpie.py show_patch --scenario scenario/minisat_GI_stmt_train_10.txt --patch path/to/my/patch
