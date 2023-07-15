19721da7697259995aa6aab3bf69271c8cefa3e3
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-15 21:40:35 +0100

    Auto stash before merge of "experiment" and "origin/experiment"

--- my-scripts/g2.sh
+++ my-scripts/g2.sh
@@ -1,8 +1,5 @@
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/AC/validate_9.txt --patch _magpie_logs/minisat-hack_1689255510.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/AC/validate_10.txt --patch _magpie_logs/minisat-hack_1689268835.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_1.txt --patch _magpie_logs/minisat-hack_1689215282.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_2.txt --patch _magpie_logs/minisat-hack_1689228744.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_3.txt --patch _magpie_logs/minisat-hack_1689242075.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_4.txt --patch _magpie_logs/minisat-hack_1689255626.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_5.txt --patch _magpie_logs/minisat-hack_1689269204.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_6.txt --patch _magpie_logs/minisat-hack_1689282573.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC/test.txt --patch _magpie_logs/minisat-hack_1689337311.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC/test.txt --patch _magpie_logs/minisat-hack_1689354186.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/GI/test.txt --patch _magpie_logs/minisat-hack_1689399178.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/GI/test.txt --patch _magpie_logs/minisat-hack_1689414513.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/GI/test.txt --patch _magpie_logs/minisat-hack_1689435082.patch
--- my-scripts/g3.sh
+++ my-scripts/g3.sh
@@ -3,4 +3,3 @@
 /usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689387799.patch
 /usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689409645.patch
 /usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689418001.patch
-
