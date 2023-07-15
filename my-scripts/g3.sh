240f5aac5835beadf36a3317b64ce03e53224e5d
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-15 21:29:21 +0100

    :page_facing_up: minisat GCC validation second-half result.

--- my-scripts/g3.sh
+++ my-scripts/g3.sh
@@ -1,7 +1,6 @@
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_7.txt --patch _magpie_logs/minisat-hack_1689188424.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_8.txt --patch _magpie_logs/minisat-hack_1689201795.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_9.txt --patch _magpie_logs/minisat-hack_1689215148.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/GI/validate_10.txt --patch _magpie_logs/minisat-hack_1689228494.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/AC_GI/validate_1.txt --patch _magpie_logs/minisat-hack_1689188416.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/AC_GI/validate_2.txt --patch _magpie_logs/minisat-hack_1689201810.patch
-/usr/local/bin/python3.10 -m bin.minify_patch --scenario scenario/AC_GI/validate_3.txt --patch _magpie_logs/minisat-hack_1689215149.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/GI/test.txt --patch _magpie_logs/minisat-hack_1689337313.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/GI/test.txt --patch _magpie_logs/minisat-hack_1689376850.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689387799.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689409645.patch
+/usr/local/bin/python3.10 -m bin.revalidate_patch --scenario scenario/AC_GI/test.txt --patch _magpie_logs/minisat-hack_1689418001.patch
+
