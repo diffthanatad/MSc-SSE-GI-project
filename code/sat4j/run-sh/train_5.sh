32279876528231a302b52929d6bad652652f8157
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-19 09:47:01 +0100

    :zap: Upload *.sh file for sat4j.

---
+++ code/sat4j/run-sh/train_5.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+ARGV=$@
+
+my_test() {
+    FILENAME=$1
+    EXPECTED=$2
+    ./simp/minisat_HACK_999ED_CSSC $FILENAME $ARGV > /dev/null
+    RETURN=$?
+    if [ $RETURN -ne $((EXPECTED)) ]
+    then
+        echo "FAILED ON FILE:" $FILENAME
+        echo "GOT:" $RETURN
+        echo "EXPECTED:" $EXPECTED
+        exit -1
+    fi
+}
+
+my_test data/fuzz_100_15380.cnf 10
+my_test data/fuzz_100_2035.cnf 10
+my_test data/fuzz_100_3086.cnf 10
+my_test data/fuzz_100_45.cnf 10
+my_test data/fuzz_100_31574.cnf 10
+
+my_test data/fuzz_100_32080.cnf 20
+my_test data/fuzz_100_827.cnf 20
+my_test data/fuzz_100_2308.cnf 20
+my_test data/fuzz_100_21470.cnf 20
+my_test data/fuzz_100_14302.cnf 20
---
+++ code/sat4j/run-sh/train_9.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+ARGV=$@
+
+my_test() {
+    FILENAME=$1
+    EXPECTED=$2
+    ./simp/minisat_HACK_999ED_CSSC $FILENAME $ARGV > /dev/null
+    RETURN=$?
+    if [ $RETURN -ne $((EXPECTED)) ]
+    then
+        echo "FAILED ON FILE:" $FILENAME
+        echo "GOT:" $RETURN
+        echo "EXPECTED:" $EXPECTED
+        exit -1
+    fi
+}
+
+my_test data/fuzz_100_19666.cnf 10
+my_test data/fuzz_100_817.cnf 10
+my_test data/fuzz_100_28085.cnf 10
+my_test data/fuzz_100_26944.cnf 10
+my_test data/fuzz_100_25064.cnf 10
+
+my_test data/fuzz_100_7973.cnf 20
+my_test data/fuzz_100_32676.cnf 20
+my_test data/fuzz_100_15554.cnf 20
+my_test data/fuzz_100_17588.cnf 20
+my_test data/fuzz_100_18812.cnf 20
