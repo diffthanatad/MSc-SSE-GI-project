542822e3c05c487c42d596f2c38f29e19ee2f95b
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-07-28 21:45:30 +0100

    :zap: Update minisat.

--- code/minisat-hack/compile.sh
+++ code/minisat-hack/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MROOT=.. CFLAGS=-fpermissive CXX=/opt/rh/devtoolset-9/root/usr/bin/g++ make -C simp
+MROOT=.. CFLAGS=-fpermissive make -C simp
--- env_setup.sh
+++
@@ -1,2 +0,0 @@
-scl enable devtoolset-9 csh
-source /opt/Python/Python-3.10.1_Setup.csh
