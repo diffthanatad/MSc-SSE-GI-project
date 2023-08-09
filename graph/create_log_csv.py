7bddcd4efa6d0da7b8f1b7dc67bc34f9e3e6b3d2
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-08-09 21:38:21 +0100

    :chart_with_upwards_trend: LS and GA result train phase CSV.
    - Remove the # from the header row.
    - Commit a modified python file for this update of CSV files.

--- graph/create_log_csv.py
+++ graph/create_log_csv.py
@@ -1,25 +1,56 @@
 import numpy as np
 from datetime import datetime
 
+def is_equal_number_of_edit(total_edit, AC, GI):
+    return int(total_edit) == (int(AC) + int(GI))
+
 def main(i, file_name, search_space, algorithm):
     rows = list()
     with open("../_magpie_logs/{}.log".format(file_name), "r") as file:
         first_line = file.readline().split()
         start_time = datetime.strptime(first_line[0]+first_line[1][:len(first_line[1])-4:],'%Y-%m-%d%H:%M:%S')
+        
+        ParamSetting = 0
+        StmtInsertion = 0
+        StmtDeletion = 0
+        StmtReplacement = 0
 
         for line in file:
-            contents = line.split()
+            contents = line.split('|')
+            possible_edit = contents[0][32:]
+            if possible_edit.startswith('ParamSetting') or possible_edit.startswith('StmtInsertion') or possible_edit.startswith('StmtDeletion') or possible_edit.startswith('StmtReplacement'):
+                ParamSetting = 0
+                StmtInsertion = 0
+                StmtDeletion = 0
+                StmtReplacement = 0
+                edits = list()
 
+                for edit in [contents[0][32:], *contents[1::]]:
+                    temp = edit.strip()
+                    if temp.startswith('ParamSetting'):
+                        ParamSetting += 1
+                    elif temp.startswith('StmtInsertion'):
+                        StmtInsertion += 1
+                    elif temp.startswith('StmtDeletion'):
+                        StmtDeletion += 1
+                    elif temp.startswith('StmtReplacement'):
+                        StmtReplacement += 1
+                continue            
+            
+            contents = line.split()
             if len(contents) == 6 and contents[3] == "INITIAL" and contents[4] == "SUCCESS":
+                """ Initial Success """
                 variant_number = -1
                 timestamp = datetime.strptime(contents[0]+contents[1][:len(contents[1])-4:],'%Y-%m-%d%H:%M:%S')
-                time_elapsed = int((timestamp - start_time).total_seconds())
+                time_elapsed = 0
                 status = "INITIAL SUCCESS"
                 fitness = int(contents[5])
                 percent = 100
                 edits = 0
-                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits])
+
+                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits, 0, 0, 0, 0])
             elif len(contents) == 9 and contents[2] == "[INFO]" and contents[4] == "SUCCESS":
+                """ Success """
                 variant_number = contents[3]                
                 timestamp = datetime.strptime(contents[0]+contents[1][:len(contents[1])-4:],'%Y-%m-%d%H:%M:%S')
                 time_elapsed = int((timestamp - start_time).total_seconds())
@@ -30,17 +61,44 @@ def main(i, file_name, search_space, algorithm):
                     fitness = int(contents[5])
                 percent = float(contents[6][1:-2:])
                 edits = contents[7][1:]
-                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits])
-            elif len(contents) == 8 and contents[2] == "[INFO]":
+                
+                if not is_equal_number_of_edit(edits, ParamSetting, (StmtInsertion + StmtDeletion + StmtReplacement)):
+                    print("NOT EQUAL:", contents)
+                    print(edits, '|', ParamSetting, StmtInsertion, StmtDeletion, StmtReplacement)
+                    exit(0)
+                
+                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits, ParamSetting, StmtInsertion, StmtDeletion, StmtReplacement])
+                ParamSetting = 0
+                StmtInsertion = 0
+                StmtDeletion = 0
+                StmtReplacement = 0
+            elif len(contents) == 8 and contents[2] == "[INFO]" and contents[3] != "Best" and contents[4] != "patch:":
+                """ Error """
                 variant_number = contents[3]                
                 timestamp = datetime.strptime(contents[0]+contents[1][:len(contents[1])-4:],'%Y-%m-%d%H:%M:%S')
                 time_elapsed = int((timestamp - start_time).total_seconds())
                 status = contents[4]
                 fitness = None
                 percent = None
-                edits = None
-                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits])
-    np.savetxt("../results/{}_{}_{}.csv".format(algorithm, search_space, i), rows, delimiter=",", fmt='% s', header="variant number, timestamp, time elapse, status, fitness, percentage, edit",)
+                edits = contents[6][1:]
+
+                # print(contents)
+                # print(edits, '|', ParamSetting, StmtInsertion, StmtDeletion, StmtReplacement)
+                # print()
+                # print()
+
+                if not is_equal_number_of_edit(edits, ParamSetting, (StmtInsertion + StmtDeletion + StmtReplacement)):
+                    print("NOT EQUAL:", contents)
+                    print(edits, '|', ParamSetting, StmtInsertion, StmtDeletion, StmtReplacement)
+                    exit(0)
+                
+                rows.append([variant_number, timestamp, time_elapsed, status, fitness, percent, edits, ParamSetting, StmtInsertion, StmtDeletion, StmtReplacement])
+                ParamSetting = 0
+                StmtInsertion = 0
+                StmtDeletion = 0
+                StmtReplacement = 0
+    
+    np.savetxt("../results/{}_{}_{}.csv".format(algorithm, search_space, i), rows, delimiter=",", fmt='% s', header="variant_number,timestamp,time_elapse,status,fitness,percentage,edit,ParamSetting,StmtInsertion,StmtDeletion,StmtReplacement", comments='')
 
 """ Genetic Algorithm """
 ga_ac_logs = [
