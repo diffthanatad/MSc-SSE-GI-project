b89b772e45f028a583a6412acfcd909289837be0
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-08-16 20:37:59 +0100

    :zap: Prepare cluster script for sat4j training all search spaces.

--- my-cluster-scripts/sat4j_train_ac_gi_all.sh
+++ my-cluster-scripts/sat4j_train_ac_gi_all.sh
@@ -1,7 +1,7 @@
 #$ -S /bin/bash
 #$ -l tmem=9G
 #$ -l h_vmem=9G
-#$ -N sat4j_train_gi_all
+#$ -N sat4j_train_ac_gi_all
 #$ -t 1-10
 #$ -j y
 
@@ -11,4 +11,4 @@ export PATH=/share/apps/openjdk-12.0.2/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/openjdk-12.0.2/lib:$LD_LIBRARY_PATH
 
 cd MSc-SSE-GI-project
-python3 -m bin.local_search --scenario scenario/sat4j/GI/train_${SGE_TASK_ID}.txt
+python3 -m bin.local_search --scenario scenario/sat4j/AC_GI/train_${SGE_TASK_ID}.txt
