634086eb3ecd2461995f13d2b81d369854d2ad72
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-08-17 21:53:07 +0100

    :construction: Update my-script for long machine.

--- my-scripts/g3.sh
+++ my-scripts/g3.sh
@@ -2,4 +2,5 @@ python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_1.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_2.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_3.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_4.txt
-python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_10.txt
+python3 -m bin.genetic_algorithm --scenario scenario/minisat/AC_GI/train_5.txt
+
--- my-scripts/g5.sh
+++ my-scripts/g5.sh
@@ -3,3 +3,4 @@ python3 -m bin.genetic_algorithm --scenario scenario/minisat/GI/train_2.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/GI/train_3.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/GI/train_4.txt
 python3 -m bin.genetic_algorithm --scenario scenario/minisat/GI/train_5.txt
+
