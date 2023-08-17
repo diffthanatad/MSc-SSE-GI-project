e8fbd2cd3f046cb79230ab5e7cfc82604f661d73
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-08-17 16:02:46 +0100

    :zap: Update cluster script for sat4j.

--- my-cluster-scripts/sat4j_train_ac_gi_all.sh
+++ my-cluster-scripts/sat4j_train_ac_gi_all.sh
@@ -1,13 +1,15 @@
 #$ -S /bin/bash
-#$ -l tmem=9G
-#$ -l h_vmem=9G
+#$ -l tmem=7.5G
+#$ -l h_vmem=7.5G
 #$ -N sat4j_train_ac_gi_all
+#$ -t 1-10
+#$ -wd /home/tsongpet/MSc-SSE-GI-project
 #$ -j y
+#$ -l hostname=burns-610-43|fry-611-118
 
 source /share/apps/source_files/python/python-3.10.0.source
 export JAVA_HOME=/share/apps/openjdk-12.0.2
 export PATH=/share/apps/openjdk-12.0.2/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/openjdk-12.0.2/lib:$LD_LIBRARY_PATH
 
-cd MSc-SSE-GI-project
-python3 -m bin.local_search --scenario scenario/sat4j/AC_GI/train_1.txt
+python3 -m bin.local_search --scenario scenario/sat4j/AC_GI/train_${SGE_TASK_ID}.txt
