2af1d1d406661f7b4e2a7d3434daa9861f066e4c
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-09-05 20:51:00 +0700

    :zap: Change GI with LS k5 script to test file.

--- my-scripts/g1.sh
+++ my-scripts/g1.sh
@@ -1,4 +1,4 @@
 #!/bin/csh
 source /opt/Python/Python-3.10.1_Setup.csh
 
-python3 -m bin.revalidate_patch --scenario scenario/minisat/GI/train_5.txt --patch _magpie_logs/minisat-hack_1691009902_copy.patch
+python3 -m bin.revalidate_patch --scenario scenario/minisat/GI/test_5.txt --patch _magpie_logs/minisat-hack_1691009902_copy.patch
