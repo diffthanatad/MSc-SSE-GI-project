de92c0c16004807ec938d6e1794d84f19e3b3621
Thanatad Songpetchmongkol <thanatad@gmail.com>
2023-09-05 19:32:37 +0700

    :construction: GI with LS k-5 return False;

--- my-scripts/g1.sh
+++ my-scripts/g1.sh
@@ -1,8 +1,4 @@
 #!/bin/csh
 source /opt/Python/Python-3.10.1_Setup.csh
 
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_1.txt --patch _magpie_logs/minisat-hack_1692606991.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_4.txt --patch _magpie_logs/minisat-hack_1692613302.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_5.txt --patch _magpie_logs/minisat-hack_1692615169.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_6.txt --patch _magpie_logs/minisat-hack_1692617047.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_7.txt --patch _magpie_logs/minisat-hack_1692618941.patch
+python3 -m bin.revalidate_patch --scenario scenario/minisat/GI/train_5.txt --patch _magpie_logs/minisat-hack_1691009902_copy.patch
--- my-scripts/g2.sh
+++ my-scripts/g2.sh
@@ -1,8 +1,2 @@
 #!/bin/csh
 source /opt/Python/Python-3.10.1_Setup.csh
-
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_8.txt --patch _magpie_logs/minisat-hack_1692620962.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_9.txt --patch _magpie_logs/minisat-hack_1692622713.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC/test_10.txt --patch _magpie_logs/minisat-hack_1692624637.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC_GI/test_3.txt --patch _magpie_logs/minisat-hack_1692611221.patch
-python3 -m bin.revalidate_patch --scenario scenario/minisat/AC_GI/test_4.txt --patch _magpie_logs/minisat-hack_1692613161.patch
